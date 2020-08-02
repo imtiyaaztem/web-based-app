9f030445d39eeed735089b2c0f429d9a05284ee2
imtiyaaztem <61545556+imtiyaaztem@users.noreply.github.com>
2020-08-02 18:11:08 +0200

    Prepare for deploy

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ var Users = require("./routes/Users");
 
 app.use("/users", Users);
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 app.listen(port, function () {
   console.log("Server is running on port: " + port);
 });
